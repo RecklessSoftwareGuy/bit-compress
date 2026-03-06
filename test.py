c00c70448dcba27350d85fb43f8323af10c0c142
RecklessSoftwareGuy <rnair5603@gmail.com>
2026-03-06 19:45:03 +0530

    Made Python file create Binary test files instead of txt

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
-#Program creates test files
 import random
+import os
 
-def create_test() -> str:
-    digits = [0, 1]
+def create_binary_test() -> bytearray:
     length = random.randint(10000, 30000)
-    print(f"Length of file: {length}")
+    rem = length % 8
+    length = length - rem
+    print(f"Length of the binary string: {length}")
     s = ""
     while length > 0:
-        s += f"{random.choice(digits)}"
-        length -=1
-    return s
+        s += f"{random.choice([0, 1])}"
+        length -= 1
+    print(f"Total file size will be: {len(s) // 8} bytes")
+    byte_array = bytearray()
+    for i in range(0, len(s), 8):
+        byte_chunk = s[i:i+8]
+        byte_val = int(byte_chunk, 2) 
+        byte_array.append(byte_val)
+    return byte_array
 
-with open('assets/test.txt', 'w') as f:
-    s = create_test()
-    f.write(s)
-    print("File created sucessfully")
+with open('assets/test.bin', 'wb') as f:
+    b_data = create_binary_test()
+    f.write(b_data)
+    print("Binary file created successfully at assets/test.bin")
